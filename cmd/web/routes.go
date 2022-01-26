71265c9d50b97f918b670fbd271f6411c86dbd24
Golda-Lee Sawyers <goldalee.sawyers@gmail.com>
2022-01-25 23:29:50 -0500

    Adding images

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -30,6 +30,10 @@ func routes(app *config.AppConfig) http.Handler {
 	//mux.Use(WriteToConsole)
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	//routing/putting the picture on your page by creating a file server
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
 	return mux
 }
 
