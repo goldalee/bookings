df65fcac11c6c771ca6114fdab98f1a5bd32a4c2
Golda-Lee Sawyers <goldalee.sawyers@gmail.com>
2022-02-08 16:41:54 -0500

    Added Go templates

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -33,6 +33,7 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/generals", handlers.Repo.Generals)
 	mux.Get("/majors", handlers.Repo.Majors)
 
+	//Availability routes
 	mux.Get("/search-availability", handlers.Repo.Availability)
 	mux.Post("/search-availability", handlers.Repo.PostAvailability)
 	mux.Get("/search-availability-json", handlers.Repo.AvailabilityJSON)
